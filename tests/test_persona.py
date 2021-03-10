c4bde4e94435d8d281161aaf9dcc5ea8a48f4a34
j.gamarra <j.gamarra@uniandes.edu.co>
2021-03-10 13:42:46 -0500

    Eliminando test_instancia_clase1

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -42,8 +42,3 @@ class PersonaTestCase(unittest.TestCase):
    def test_instancia_clase(self):
       self.assertIsInstance(self.persona1, Persona)
       self.assertNotIsInstance(self.grupo, Persona)
-
-
-   def test_instancia_clase1(self):
-      self.assertIsInstance(self.persona1, Persona)
-      self.assertNotIsInstance(self.grupo, Persona)
